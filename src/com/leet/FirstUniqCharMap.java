28e5d04edc86dc6f2e05f5bdab8fcf59decbfbd5
Camilo  Hernandez <camilo.hernandezmarin@jumbo.com>
2019-05-08 09:37:00 +0200

    second sol for first char

--- src/com/leet/FirstUniqCharMap.java
+++ src/com/leet/FirstUniqCharMap.java
@@ -7,30 +7,11 @@ import java.util.Map;
 public class FirstUniqCharMap {
 
     public static void main(String[] args) {
-        System.out.println(new FirstUniqCharMap().firstUniqChar("leetcode"));
+        System.out.println(new FirstUniqCharMap().firstUniqChar("eetcode"));
     }
 
     public int firstUniqChar(String s) {
 
-        char[] chars = s.toCharArray();
-        int time = 1;
-        Map<Character,Integer> map = new LinkedHashMap<Character, Integer>();
-        Map<Character,Integer> mapItem = new HashMap<Character,Integer>();
-        for (int i = 0; i < chars.length; i++){
-            if (map.containsKey(chars[i])){
-                int val = map.get(chars[i]);
-                ++val;
-                map.put(chars[i],val);
-            }else{
-                map.put(chars[i],time);
-            }
-            mapItem.put(chars[i],i);
-        }
-        for (Map.Entry<Character,Integer> entry : map.entrySet()){
-            if (entry.getValue() == 1){
-                return mapItem.get(entry.getKey());
-            }
-        }
         return -1;
 
     }
