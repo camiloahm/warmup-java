297400b36a393ca0d524a545570f011260b5c67a
Camilo  Hernandez <camilo.hernandezmarin@jumbo.com>
2019-12-02 17:04:01 +0100

    Single list

---
+++ src/com/crack/Node.java
@@ -0,0 +1,43 @@
+package com.crack;
+
+public class Node {
+
+    Node next;
+    int data;
+
+    public Node(int data) {
+        this.data = data;
+    }
+
+    void appendToTail(int newValue) {
+
+        Node newEndNode = new Node(newValue);
+        Node n = this;
+
+        while (n.next != null) {
+            n = n.next;
+        }
+
+        n.next = newEndNode;
+    }
+
+    Node removeNode(Node head, int d) {
+
+        Node n = head;
+
+        if (n.data == d) {
+            return head.next;
+        }
+
+        while (n.next != null) {
+            if (n.next.data == data) {
+                n.next = n.next.next;
+                return head;
+            }
+            n = n.next;
+        }
+
+        return head;
+    }
+
+}
